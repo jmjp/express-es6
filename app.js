2c744aeb6ab12e1c0b742112cf898257ceaee5a6
João Miguel <joaomigguel@icloud.com>
2021-09-13 14:23:42 -0300

    remove body-parser)

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import bodyParser from "body-parser";
 import routes from './src/routes';
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/api',routes);
 
 app.listen(process.env.PORT, () => {
