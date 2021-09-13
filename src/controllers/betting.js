b313b8476ed9f3d05f38c5bffe9af8f445774a37
João Miguel <joaomigguel@icloud.com>
2021-09-13 16:11:08 -0300

    updateds

--- src/controllers/betting.js
+++ src/controllers/betting.js
@@ -3,6 +3,8 @@ import * as playerService from '../services/players';
 
 async function checkPost(req,res){
     var betting  = req.body.event.data.new;
+    console.log(betting);
+    console.log(betting.user);
     var points  = await playerService.FindUserPointsById(betting.user);
     console.log(points);
     await playerService.UpdateUserPoints(betting.user,(points.data.main_users[0].points - betting.amout));
