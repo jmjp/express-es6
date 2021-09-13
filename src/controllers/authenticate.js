a8ebbfc5d74d44de182ef380cfc2234e0684c92f
João Miguel <joaomigguel@icloud.com>
2021-09-13 14:16:15 -0300

    log req

--- src/controllers/authenticate.js
+++ src/controllers/authenticate.js
@@ -40,8 +40,7 @@ const register = async (req,res) => {
 }
 
 const login = async (req,res) => {
-    console.log(req.body);
-    var {identifier, password, } = req.body;
+    var {identifier, password, } = req.body.input;
     if(identifier == undefined){
         return res.status(400).json({message: "invalid identifier"});
     }
