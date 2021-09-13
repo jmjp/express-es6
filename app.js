08667057475ec689cb44929425f3305cb887c6fa
João Miguel <joaomigguel@icloud.com>
2021-09-13 13:28:53 -0300

    remove env

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import bodyParser from "body-parser";
 import routes from './src/routes';
 const app = express();
-process.env.PORT = 8080;
-process.env.SECRET_KEY = "linux123";
 
 app.use(bodyParser.json());
 app.use('/api',routes);
