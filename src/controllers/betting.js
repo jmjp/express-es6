c08164e12f0bff13267c4d4fd3e01905c0a4ea5c
João Miguel <joaomigguel@icloud.com>
2021-09-13 17:28:56 -0300

    updated

--- src/controllers/betting.js
+++ src/controllers/betting.js
@@ -1,4 +1,5 @@
 import * as playerService from '../services/players';
+import * as bettingService from '../services/betting';
 import { FindAvaibleRoulette } from '../services/roulette'; 
 
 
@@ -11,18 +12,25 @@ async function reducePoints(req,res){
 }
 
 async function createBetting(req,res){
-    console.log(JSON.stringify(req.body));
     const bettingContent = req.body.input;
     const currentUser = req.body.session_variables["x-hasura-user-id"];
     var roulette = await FindAvaibleRoulette();
+    if(roulette.data.games_roulette[0] == undefined){
+        return res.status(400).json({message: "no roulette avaible"});
+    }
+    var points = await playerService.FindUserPointsById(currentUser);
+    if(points.data.main_users[0].points < bettingContent.amount){
+        return res.status(400).json({message: "you don't have points"});
+    }
     var betting = {
-        amout: bettingContent.amout,
+        amout: bettingContent.amount,
         game_id: roulette.data.games_roulette[0],
         user: currentUser,
         selected: bettingContent.selected,
         payout_multiplier: bettingContent.selected == 'Green' ? 14 : 2
     }
-    console.log(betting);
+    var createdBetting = await bettingService.CreateBetting(betting.amout,betting.user, betting.selected, betting.game_id, betting.payout_multiplier);
+    console.log(createdBetting);
     return res.json({
         amout: 5,
         game_id: 1,
---
+++ src/services/betting.js
@@ -0,0 +1,32 @@
+const createBetting = `
+mutation CreateBetting($amout: float8, $user: Int, $selected: games_betting_selected_enum, $game: Int,$multi: Int) {
+    insert_games_betting(objects: {amout: $amout, user: $user, selected: $selected, game_id: $game, payout_multiplier: $multi}) {
+      returning {
+        amout
+        selected
+        payout_multiplier
+        game_id
+      }
+    }
+  }
+  
+
+`;
+
+const execute = async (variables, query) => {
+    const response = await axios.post("https://versus-betting.herokuapp.com/v1/graphql", {
+        query: query,
+        variables
+    },
+    {
+        headers:{
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET
+        }
+      })
+    return { data: response.data };
+};
+
+export const CreateBetting = async (amount,user,selected,game,multi) => {
+    var { data } = await execute({ amount,user,selected,game,multi }, createBetting);
+    return data;
+}
