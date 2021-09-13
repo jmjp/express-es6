22b2c7fe07c30216bc23896d50db53b4531a5a87
João Miguel <joaomigguel@icloud.com>
2021-09-13 15:32:18 -0300

    trigger test

--- src/controllers/betting.js
+++ src/controllers/betting.js
@@ -1,5 +1,5 @@
 async function checkPost(req,res){
-    console.log(JSON.stringify(req.body));
+    console.log(JSON.stringify(req.body.event.data.new));
     return res.json({message: "teste"});
 }
 
